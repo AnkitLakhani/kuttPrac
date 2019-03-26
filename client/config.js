20cb43338a7bfd13f9abfa167f4d5d013c79c8f8
AnkitLakhani <ankitlakhani52@gmail.com>
2019-03-26 11:02:08 +0000

    Create config.js

---
+++ client/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /*
+    Invisible reCaptcha site key
+    Create one in https://www.google.com/recaptcha/intro/
+  */
+  RECAPTCHA_SITE_KEY: '6LfA_5kUAAAAAIB1rk0RxXJdTiIUA5lxuD7OhqUF',
+
+  // Google analytics tracking ID
+  GOOGLE_ANALYTICS_ID: 'UA-136764701-1',
+
+  // Contact email address
+  CONTACT_EMAIL: '',
+
+  // Report email address
+  REPORT_EMAIL: '',
+};
