62212965db7bf8f9e57fe89d93bb1820fcd7933a
AnkitLakhani <ankitlakhani52@gmail.com>
2019-03-27 05:04:37 +0000

    Update config.js

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT,
 
   /* The domain that this website is on */
-  DEFAULT_DOMAIN: 'frozen-brushlands-16834.herokuapp.com',
+  DEFAULT_DOMAIN: 'afternoon-temple-49549.herokuapp.com',
 
   /* Neo4j database credential details */
   DB_URI: 'bolt://hobby-hljjgcdlapbogbkekgjfppcl.dbs.graphenedb.com:24787',
